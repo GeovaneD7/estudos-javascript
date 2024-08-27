e58226f7aa2c148db7dc74ef56f8413f63486f58
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-08-27 02:22:47 -0300

    Resolvido o problema do tabuleiro de xadrez.

--- exercicios/Eloquent JavaScript/chessboard.js
+++ exercicios/Eloquent JavaScript/chessboard.js
@@ -4,3 +4,19 @@
  loop1 - recebe os 2 valores e inicia o loop de impressão
      loop2 */
 
+const tamanho = 8;
+
+let grid = "";
+
+for (let linha = 0; linha < tamanho; linha++){
+    for (let coluna = 0; coluna < tamanho; coluna++) {
+        if ((linha + coluna) % 2 == 0) {
+            grid += " ";
+        } else {
+            grid += "#";
+        };
+    };
+    grid += "\n";
+};
+
+console.log(grid);
