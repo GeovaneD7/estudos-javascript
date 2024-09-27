8d914a65f0aed4117b66672120977f293e9e2bea
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-09-27 19:09:50 -0300

    resolvido o exercício de recurção.

--- exercicios/Eloquent JavaScript/recursion.js
+++ exercicios/Eloquent JavaScript/recursion.js
@@ -1,13 +1,20 @@
 // isEven 
 
 function isEven(numero) {
-    let absolute = Math.abs(nummero);
+    let absolute = Math.abs(numero);
     if ((absolute % 2) === 0 ) {
         return true;
     } else if ((absolute % 2) === 1) {
         return false;
     } else {
-
+        isEven((absolute - 2))
     }
-    
-}
+};
+
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
